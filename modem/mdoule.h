3a9e8506ae0134c871472766b13b8bdfd89ff7ae
Altamash <altamash.ar96@gmail.com>
2019-08-01 20:19:49 +0530

    First Commit

---
+++ modem/mdoule.h
@@ -0,0 +1,13 @@
+/*
+ * mdoule.h
+ *
+ *  Created on: Aug 1, 2019
+ *      Author: Heeze
+ */
+
+#ifndef MDOULE_H_
+#define MDOULE_H_
+
+
+
+#endif /* MDOULE_H_ */
---
+++ modem/module.c
@@ -0,0 +1,13 @@
+/*
+ * module.c
+ *
+ *  Created on: Aug 1, 2019
+ *      Author: Heeze
+ */
+
+#ifndef MODULE_C_
+#define MODULE_C_
+
+
+
+#endif /* MODULE_C_ */
